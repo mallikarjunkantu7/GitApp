fa134d8e39318e6363f1f9a76857e9f9cd7e5705
mallikarjunkantu7 <121636795+mallikarjunkantu7@users.noreply.github.com>
2024-11-07 06:13:29 +0000

    revert

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,23 +3,12 @@ import streamlit as st
 st.title("🎈GitBot")
 # st.markdown("JuggBot :juggernaut:")
 
-# Create two columns
-col1, col2 = st.columns(2)
-
-# Display text in the first column
-with col1:
-    st.header("Header")
-    # st.write("Here is some text next to the image. You can add more content here.")
-
-# Display image in the second column
-with col2:
-    st.image("/workspaces/GitApp/jugg_image.png", caption="Cute Cat", width=300)  
-
 
 st.write(
-    "App to search the commit history"
+    "OnePlace to search the GITHUB commit history"
 )
-# st.image('/workspaces/GitApp/jugg_image.png')
+
+st.image('/workspaces/GitApp/jugg_image.png')
 
 st.sidebar.title("Home")
 
