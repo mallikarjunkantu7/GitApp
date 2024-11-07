14c5a345a762cc0935ef01fefc5e41e1ea14ce5e
mallikarjunkantu7 <121636795+mallikarjunkantu7@users.noreply.github.com>
2024-11-07 00:19:17 +0000

    add drop down

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,7 @@ st.write(
 
 # Create a selectbox (dropdown) with options
 selected_option = st.selectbox(
-    'Choose a color:',
-    ['Red', 'Green', 'Blue', 'Yellow']
+    'Choose a repository:',
+    # ['Red', 'Green', 'Blue', 'Yellow']
 )
 
-# Display the selected option
-st.write(f'You selected: {selected_option}')
