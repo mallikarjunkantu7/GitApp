c51c2b4c2fa9cabcf88d2f08f02bd1e9607f3e6d
mallikarjunkantu7 <121636795+mallikarjunkantu7@users.noreply.github.com>
2024-11-07 19:28:28 +0000

    minor change

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,24 +3,41 @@ from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
-st.title("GitBot")
-# st.markdown("JuggBot :juggernaut:")
+# st.title("GitBot")
+# # st.markdown("JuggBot :juggernaut:")
 
 
-st.write(
-    "OnePlace to search the GITHUB commit history"
-)
+# st.write(
+#     "OnePlace to search the GITHUB commit history"
+# )
 
-st.image('/workspaces/GitApp/jugg_image.png')
+# st.image('/workspaces/GitApp/jugg_image.png')
 
-st.sidebar.title("Home")
+# st.sidebar.title("Home")
 
 
-# Create a selectbox (dropdown) with options
-selected_option = st.sidebar.selectbox(
-    'Choose a GitHub repository:',
-    ['Red', 'Green', 'Blue', 'Yellow']
-)
+# # Create a selectbox (dropdown) with options
+# selected_option = st.sidebar.selectbox(
+#     'Choose a GitHub repository:',
+#     ['Red', 'Green', 'Blue', 'Yellow']
+# )
 
-# if __name__ == "__main__":
-#     run()
+def run():
+    st.set_page_config(
+        page_title="Home",
+        page_icon="👋",
+    )
+
+    st.write("# Juggernaut Bot👋")
+    # st.image('jugg_image.jpg')
+
+    st.write("""
+            Please go to the sidebar and:
+             - Load some data
+             - Use the time machine             
+             """)
+
+
+if __name__ == "__main__":
+    run()
+
